561735a8d50b0c33fe362b42466a6e3149119289
Parth Sharma <parthsharma9sharma@gmail.com>
2026-02-24 20:13:09 +0530

    link fix

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,5 @@
 export { Login, Register, Onboarding } from "./auth";
 export { ParticipantDashboard, Profile, OrganizersListing, OrganizerDetail } from "./participant";
 export { BrowseEvents, EventDetails } from "./events";
-export { OrganizerDashboard, EventForm, EventParticipants, AttendanceDashboard, OrganizerProfile, OngoingEvents, OrganizerEventDetail } from "./organizer";
+export { OrganizerDashboard, EventForm, EventParticipants, AttendanceDashboard, OrganizerProfile, OngoingEvents, OrganizerEventDetail, MerchandiseOrders } from "./organizer";
 export { AdminDashboard, ManageOrganizers, OrganizerForm, PasswordRequests } from "./admin";
